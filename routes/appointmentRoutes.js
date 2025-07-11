370a27e11402e527b1d16da642dea737caadcd0e
Danielle Rochas <poetyzaa@gmail.com>
2025-07-11 19:15:43 -0300

    fix: corrigir import do middleware de autenticação

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const appointmentController = require('../controllers/appointmentController');
-const authMiddleware = require('../middlewares/auth');
+const authMiddleware = require('../middleware/auth');
 
 router.post('/', authMiddleware, appointmentController.createAppointment);
 router.get('/', authMiddleware, appointmentController.listAppointments);
